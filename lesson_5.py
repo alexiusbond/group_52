c1c5d3a837157644e58b92f1a50ec50a332c5e9c
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2025-03-13 18:31:57 +0600

    lesson 5

---
+++ lesson_5.py
@@ -0,0 +1,25 @@
+# import random
+#
+# print(random.randint(2, 10))
+from email.policy import default
+from random import randint as random_number, choice
+
+from termcolor import cprint
+from decouple import config
+
+import utilities.calculator as calc
+from utilities.templates import Person
+
+print(choice([1, 2, 5, 8, 0]))
+print(random_number(10, 100))
+
+print(calc.sub(10, 4))
+
+p1 = Person('Bill', 20)
+print(p1)
+
+cprint("Hello, World!", "green", "on_red")
+
+print(config('DATABASE_URL'))
+commented = config('COMMENTED', default=0, cast=int)
+print(commented * 2)
